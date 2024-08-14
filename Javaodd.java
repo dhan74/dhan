afaa388081184777cad3e93b57ca6708bd3779ad
dhan74 <dhanushdhanush01542@gmail.com>
2024-08-13 23:28:04 -0700

    Add files via upload

---
+++ Javaodd.java
@@ -0,0 +1,17 @@
+package corejava;
+
+public class Javaodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int b=1;b<=50;b++)
+		{
+			if(b%2==1)
+			{
+				System.out.println("print odd numbers upto 50:"+b);
+			}
+		}
+
+	}
+
+}
