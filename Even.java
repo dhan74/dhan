28442581d205e2911495dcd41113c251bf580be3
dhan74 <dhanushdhanush01542@gmail.com>
2024-08-13 23:27:36 -0700

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class Even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int a=1;a<=50;a++)
+		{
+			if(a%2==0)
+			{
+				System.out.println("print even numbers upto 50="+a);
+			}
+		}
+
+	}
+
+}
+
+
